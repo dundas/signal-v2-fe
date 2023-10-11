9d2f2ea0bb5261da36379d881e817463f3cf266b
dundas <git@daviddundas.com>
2023-10-10 19:57:45 -0500

    update caching

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import NavHeader from '@/components/Nav/NavHeader';
+export const dynamic = 'force-dynamic'
 
 const inter = Inter({ subsets: ['latin'] })
 
