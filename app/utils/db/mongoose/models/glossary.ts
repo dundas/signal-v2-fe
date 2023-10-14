c7fe40ef14f3daf5b8c947d6896b56764166c3f5
dundas <git@daviddundas.com>
2023-10-14 12:23:28 -0500

    rule, glossary models

---
+++ app/utils/db/mongoose/models/glossary.ts
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const GlossarySchema = new Schema({
+    term: {
+        type: String,
+        required: [true, 'Term is required.'],
+    },
+    definition: {
+        type: String,
+        required: [true, 'Definition is required.'],
+    },
+    examples: {
+        type: [String],
+    },
+    embedding: {
+        type: [Number],
+    },
+    createdBy: {
+        type: String
+    }
+});
+
+const Glossary = mongoose.models.Glossary || mongoose.model('Glossary', GlossarySchema);
+
+module.exports = Glossary;
---
+++ app/utils/db/mongoose/models/rules.ts
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const RulesSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Rule name.'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required.'],
+    },
+    correctQuery: {
+        type: String,
+        required: [true, 'Correct query is required.'],
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active',
+    },
+    embedding: {
+        type: [Number],
+    },
+    summary: {
+        type: String,
+    },
+}, {
+    timestamps: true // This will add `createdAt` and `updatedAt` fields
+});
+
+const Rules = mongoose.models.Rules || mongoose.model('Rules', RulesSchema);
+
+module.exports = Rules;
